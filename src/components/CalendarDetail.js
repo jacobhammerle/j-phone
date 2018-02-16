ef0924900b334aa72e51a83180ff4c5fc086bbbc
Jacob Hammerle <hammerja@miamioh.edu>
2018-02-16 18:29:22 -0500

    added CalendarDetail component

---
+++ src/components/CalendarDetail.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class CalendarDetail extends Component {
+	render() {
+		return (
+			<Text>
+				Hello World
+			</Text>
+		)
+	}
+}
+
+export default CalendarDetail;
--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,52 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import CalendarView from './CalendarView';
+import CalendarDetail from './CalendarDetail';
 
 class HomeScreen extends Component {
 	render() {
 		return (
-				<Calendar
-				  // Specify style for calendar container element. Default = {}
-				  style={{
-				    borderWidth: 1,
-				    borderColor: 'gray',
-				    height: 350
-				  }}
-				  // Specify theme properties to override specific styles for calendar parts. Default = {}
-				  theme={{
-				    backgroundColor: '#ffffff',
-				    calendarBackground: '#ffffff',
-				    textSectionTitleColor: '#b6c1cd',
-				    selectedDayBackgroundColor: '#00adf5',
-				    selectedDayTextColor: '#ffffff',
-				    todayTextColor: '#00adf5',
-				    dayTextColor: '#2d4150',
-				    textDisabledColor: '#d9e1e8',
-				    dotColor: '#00adf5',
-				    selectedDotColor: '#ffffff',
-				    arrowColor: 'orange',
-				    monthTextColor: 'blue',
-				    textDayFontSize: 16,
-				    textMonthFontSize: 16,
-				    textDayHeaderFontSize: 16
-				  }}
-				  // Initially visible month. Default = Date()
-				  current={'2018-02-15'}
-				  // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-				  minDate={'2018-02-01'}
-				  // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-				  maxDate={'2018-05-30'}
-				  // Handler which gets executed on day press. Default = undefined
-				  onDayPress={(day) => {console.log('selected day', day)}}
-				  // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
-				  monthFormat={'MMMM'}
-				  hideArrows={false}
-				  hideExtraDays={false}
-				  disableMonthChange={false}
-				  firstDay={1}
-				  // Hide day names. Default = false
-				  hideDayNames={false}
-				/>
+			<CalendarView />
+			<CalendarDetail />
 		)
 	}
 }
