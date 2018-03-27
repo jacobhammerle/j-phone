a907814d3411b0e206d19395925066caa819f711
Jacob Hammerle <hammerja@miamioh.edu>
2018-03-27 14:51:30 -0400

    successfully fired text message to phone

--- journal-call/functions/request_call.js
+++ journal-call/functions/request_call.js
@@ -1,31 +1,23 @@
-const admin = require('firebase-admin');
-const twilio = require('./twilio');
+const admin = require('firebase-admin')
+const twilio = require('./twilio')
 
-module.exports = function(req, res) {
-	if(!req.body.phone) {
-		return res.status(422).send({ error: "You must provide a phone number" });
-	}
+module.exports = (req, res) => {
+  if (!req.body.phone) {
+    return res.status(422).send({ error: 'You must provide a phone number' })
+  }
 
-	const phone = String(req.body.phone).replace(/[^\d]/g, '');
+  const phone = String(req.body.phone).replace(/[^\d]/g, '')
 
-	admin.auth().getUser(phone)
-		.then(userRecord => {
-			const code = Math.floor(Math.random() * 8999 + 1000);
+	twilio.messages.create({
+	    body: `This is yo boy Jacob`,
+	    to: phone,
+	    from: '+18598881609'
+	  }, err => {
+	    if (err) { return res.status(422).send(err) }
 
-			twilio.messages.create({
-				body: 'your code is ' + code,
-				to: phone,
-				from: '+18598881609'
-			}, (err) => {
-				if(err) { return res.status(422).send(err); }
-
-				admin.database().ref('/users/Qxfukr8RGKh3hhM9FWW3eC0SW8Y2/' + phone)
-					.update({ code: code, codeValid: true }, () => {
-						res.send({ success: true })
-					});
-			})
-		})
-		.catch((err) => {
-			res.status(422).send({ error: err });
-		})
+	    admin.database().ref(`phone/${phone}`)
+	      .update({ phone, codeValid: true }, () => {
+	        res.send({ success: true })
+	      })
+	  })
 }
